9200bd0eecdd2585e93d2b11ff0a9b824c737570
mr.ahmedmamdouh <mr.ahmedmamdouh@hotmail.com>
2021-05-05 20:09:03 +0200

    Hilt Dependency Injection and add xml related files

---
+++ app/src/main/java/com/example/taagerapp/di/AppModule.kt
@@ -0,0 +1,78 @@
+package com.example.taagerapp.di
+
+import com.example.taagerapp.BuildConfig
+import com.example.taagerapp.requests.ApiInterface
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(interceptor: HttpLoggingInterceptor) : OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGsonConverterFactory(): GsonConverterFactory = GsonConverterFactory.create()
+
+    @Provides
+    @Singleton
+    fun provideCoroutinesCallAdapterFactory() = CoroutineCallAdapterFactory()
+
+
+    @Provides
+    @Singleton
+    fun provideRetrofitBuilder(
+        gsonConverterFactory: GsonConverterFactory,
+        coroutineCallAdapterFactory: CoroutineCallAdapterFactory,
+        okHttpClient: OkHttpClient
+    ): Retrofit = Retrofit.Builder()
+        .baseUrl(BuildConfig.BASE_URL)
+        .addConverterFactory(gsonConverterFactory)
+        .client(okHttpClient)
+        .addCallAdapterFactory(coroutineCallAdapterFactory)
+        .build()
+
+    @Provides
+    fun providesLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideInterceptor() = Interceptor { chain ->
+        val request = chain.request()
+            .newBuilder()
+            .url(
+                chain.request()
+                    .url
+                    .newBuilder()
+                    .build()
+            )
+            .build()
+        return@Interceptor chain.proceed(request)
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiInterface(retrofit: Retrofit): ApiInterface =
+        retrofit.create(ApiInterface::class.java)
+}
