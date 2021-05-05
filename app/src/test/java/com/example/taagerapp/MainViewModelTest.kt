d93fbcdc899f217432c226e1c8b9c9f946acb085
mr.ahmedmamdouh <mr.ahmedmamdouh@hotmail.com>
2021-05-05 20:30:57 +0200

    Unit Test the viewmodel fetching data method

---
+++ app/src/test/java/com/example/taagerapp/MainViewModelTest.kt
@@ -0,0 +1,87 @@
+package com.example.taagerapp
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.example.taagerapp.model.Product
+import com.example.taagerapp.model.repository.MainRepository
+import com.example.taagerapp.requests.ApiInterface
+import com.example.taagerapp.ui.viewmodel.MainViewModel
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.timeout
+import org.mockito.MockitoAnnotations
+import org.mockito.junit.MockitoJUnit
+
+
+@RunWith(JUnit4::class)
+class MainViewModelTest {
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var coroutineRule = CoroutineTestRule()
+
+    @Rule
+    @JvmField
+    val rule = MockitoJUnit.rule()!!
+
+    @Mock
+    lateinit var api: ApiInterface
+    private lateinit var mainRepository: MainRepository
+    private lateinit var mainViewModel: MainViewModel
+    private lateinit var weatherObserver: Observer<List<Product>>
+
+    @ExperimentalCoroutinesApi
+    private val testDispatcher = TestCoroutineDispatcher()
+
+    @ExperimentalCoroutinesApi
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+        Dispatchers.setMain(testDispatcher)
+        mainRepository = MainRepository(api)
+        mainViewModel = MainViewModel(
+            mainRepository,
+            testDispatcher
+        )
+        weatherObserver = mock()
+    }
+
+    @ExperimentalCoroutinesApi
+    @After
+    fun after() {
+        Dispatchers.resetMain()
+        testDispatcher.cleanupTestCoroutines()
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun testApi() = testDispatcher.runBlockingTest {
+        val responseList: MutableList<Product> = mutableListOf()
+        val product = Product("1", 1618928797, "name 1", 6)
+        responseList.add(product)
+        Mockito.`when`(api.getProductsAsync()).thenReturn(responseList)
+
+        mainViewModel.repositoriesLiveData.observeForever(weatherObserver)
+        mainViewModel.fetchProducts()
+        delay(40)
+        verify(weatherObserver, timeout(50)).onChanged(responseList)
+    }
+}
